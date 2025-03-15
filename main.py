bcabf413ca770f4a0ecaa240552f8ca025879551
Islam-abdelwahed <100740366+Islam-abdelwahed@users.noreply.github.com>
2025-03-16 01:13:25 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open('static/index.html', 'r') as file:
+            self.wfile.write(file.read().encode())
+
+# For Vercel deployment
+def vercel_handler(request):
+    handler = Handler()
+    return handler
