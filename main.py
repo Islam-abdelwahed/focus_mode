d09fafbaaa2d1cd0bf1f657f34ee09eeb6ddb9cf
Islam-abdelwahed <100740366+Islam-abdelwahed@users.noreply.github.com>
2025-03-16 01:15:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        with open('static/index.html', 'r') as file:
+        with open('index.html', 'r') as file:
             self.wfile.write(file.read().encode())
 
 # For Vercel deployment
